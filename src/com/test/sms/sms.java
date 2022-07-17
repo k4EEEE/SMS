0c047920e8fa15e357163f506bc1bcdae0153c86
K3EEE <k_emam@hotmail.com>
2022-07-17 21:13:33 +0300

    second version

--- src/com/test/sms/sms.java
+++ src/com/test/sms/sms.java
@@ -5,6 +5,8 @@ public class sms {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from SMS");
+		
+		System.out.println("Version II");
 	}
 
 }
